a9dfbbd20135ef27a54a49706231f8a13ee4da73
Lokesh200129 <118580452+Lokesh200129@users.noreply.github.com>
2026-01-22 12:54:42 +0530

    padding fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import InvoiceCard from "../component/InvoiceCard";
 
 export default function Home() {
   return (
-    <div className="grid lg:grid-cols-2 grid-cols-1 p-8  gap-8 ">
+    <div className="grid lg:grid-cols-2 grid-cols-1 p-2  md:p-8   gap-8 ">
       <Form />
       <div className=" printable-area">
         <InvoiceCard />
